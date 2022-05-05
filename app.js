f3118d327f102cf548531c76e3282760d64c4e42
ramybr <99534487+ramybr@users.noreply.github.com>
2022-05-05 18:47:20 +0100

    Video_background
    Preloader animation + controlled video background

---
+++ app.js
@@ -0,0 +1,53 @@
+// Global Variables
+const video = document.querySelector(".video-container")
+const switchBtn = document.querySelector(".switch-btn")
+const loader = document.querySelector(".loader")
+const main = document.querySelector('.main')
+const header = document.querySelector('header')
+
+
+
+
+// Functions
+// Loading Screen
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0
+        loader.style.display = 'none'
+        main.style.display = 'block'
+        setTimeout(() => main.style.opacity = 1, 50)
+        changeBg()
+
+    }, 4000)
+}
+
+// Changing Opacity
+function changeBg() {
+    header.style.setProperty("--bg", "rgba(0, 0, 0, 0.7")
+}
+
+// Sliding Button
+function playPause() {
+    if (!switchBtn.classList.contains('slide')) {
+        switchBtn.classList.add('slide')
+        video.pause()
+    } else {
+        switchBtn.classList.remove('slide')
+        video.play()
+    }
+
+}
+
+
+// Event Listeners
+switchBtn.addEventListener("click", playPause)
+
+
+
+
+
+
+
+
+
+init();
